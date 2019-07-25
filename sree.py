046840dc97ae8f700520e2d56f3831c707fa93c0
Sreehari Radhakrishnan <sreehari.rk@gmail.com>
2019-07-25 15:49:45 +0530

    Second commit

--- sree.py
+++ sree.py
@@ -3,7 +3,7 @@ side1 = float(input('Enter the first side of a triangle: '))
 side2 = float(input('Enter the second side of a triangle: '))
 side3 = float(input('Enter the third side of a triangle: '))
 S = (side1+side2+side3)/2
-# calculate the area
+#Sreehari Triangle
 area = (S*(S-side1)*(S-side2)*(S-side3)) ** 0.5
 print('The area of the triangle is: ',area)
 
